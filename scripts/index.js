fd4d2fdd70e0bcd7649710b27e90a3da85dac16e
Marina Sorokina <Sorokina2525@yandex.ru>
2023-02-14 21:21:51 +0400

    исправления-вторая-итерация

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,12 @@ const inputName = popupForm.querySelector('.popup__input_type_name');
 const inputProfession = popupForm.querySelector('.popup__input_type_profession');
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
-const inputButtonSave = popupForm.querySelector('.popup__button-save');
 
 function handleFormSubmit (event) {
     event.preventDefault();
     profileName.textContent = inputName.value;
     profileProfession.textContent = inputProfession.value;
+    closePopup();
 }
 
 function openPopup () {
@@ -27,7 +27,6 @@ function closePopup () {
 popupForm.addEventListener('submit', handleFormSubmit);
 editButton.addEventListener('click', openPopup);
 popupButtonExit.addEventListener('click', closePopup);
-inputButtonSave.addEventListener('click', closePopup);
 
 
 
